511e7441841496df990a8d98e7172f65bb8e559e
Denis Korobicyn <deniskorobicyn@users.noreply.github.com>
2022-02-09 00:01:17 +0500

    add axios call to go app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    async getAllData() {
+      const res = await axios.get(`http://localhost:3000/challenges`);
 
+      this.items = res.data;
+    },
+  },
+};
 </script>
 
 <template>
-  <main>
-    <p> Hello, world! </p>
-  </main>
+  <button @click="getAllData">Get All</button>
+  <li v-for="(item, index) in items">
+    {{ index }} - {{ item.name }} - {{ item.description }} -  {{ item.created_at }}
+  </li>
 </template>
